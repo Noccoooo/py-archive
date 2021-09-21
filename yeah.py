ea7ae3e899bc49abbfa62766cfc880d2ba7fe15c
Nocco <91106143+Noccoooo@users.noreply.github.com>
2021-09-21 11:24:28 +0700

    Add files via upload

---
+++ yeah.py
@@ -0,0 +1,19 @@
+# basically exercises i did for a python tutorial video
+#Specifically, https://www.youtube.com/watch?v=_uQrJ0TkZlc
+
+# this file is perfect for summarizing the tutorial
+
+# this is creating a variable that stores stuff from the input function
+name = input("What's your name? ")
+color = input("What's your favorite color? ")
+
+# basically prints the stuff stored in the variables
+print(f"{name} likes {color}")
+
+# Pounds to kilograms calculator, the results are rounded up.
+weight = input("What's your weight? (lbs/pounds) ")
+
+print(str(int(weight) // 2.205) + " (This is the ROUNDED answer)")
+print(str(int(weight) / 2.205) + " (This is the UNROUNDED answer)")
+
+#im too lazy to complete this shit LMAO
